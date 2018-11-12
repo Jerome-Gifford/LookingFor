be28d46e2a370c11210b076c5bcdaf37097fa25f
Brady Dufresne <bjdufre1@svsu.edu>
2018-11-11 21:51:45 -0500

    Add Event class and capibility to add event from create event

--- app/src/main/java/lf/lookingfor/CreateEventFragment.java
+++ app/src/main/java/lf/lookingfor/CreateEventFragment.java
@@ -7,9 +7,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CreateEventFragment extends Fragment {
+    EditText event_name;
+    EditText event_date;
+    EditText start_time;
+    EditText end_time;
+    EditText min_number;
+    EditText max_number;
+    EditText min_age;
+    EditText max_age;
+    EditText event_desc;
+    EditText event_address;
+    EditText event_city;
+    EditText event_state;
+    EditText event_ZIP;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,15 +37,44 @@ public class CreateEventFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onViewCreated(@NonNull final View mainView, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(mainView, savedInstanceState);
 
-        view.findViewById(R.id.create_event).setOnClickListener(new View.OnClickListener() {
+        mainView.findViewById(R.id.create_event).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-
+                createEvent(mainView);
                 Toast.makeText(getActivity(), "Create Event!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void createEvent(View view){
+        event_name = (EditText) view.findViewById(R.id.event_name);
+        event_date= (EditText) view.findViewById(R.id.event_date);
+        start_time= (EditText) view.findViewById(R.id.start_time);
+        end_time= (EditText) view.findViewById(R.id.end_time);
+        min_number= (EditText) view.findViewById(R.id.min_number);
+        max_number= (EditText) view.findViewById(R.id.max_number);
+        min_age= (EditText) view.findViewById(R.id.min_age);
+        max_age= (EditText) view.findViewById(R.id.max_age);
+        event_desc= (EditText) view.findViewById(R.id.event_desc);
+        event_address= (EditText) view.findViewById(R.id.event_address);
+        event_city= (EditText) view.findViewById(R.id.event_city);
+        event_state= (EditText) view.findViewById(R.id.event_state);
+        event_ZIP= (EditText) view.findViewById(R.id.event_ZIP);
+        Event event = new Event(
+                event_name.getText().toString().trim(),
+                start_time.getText().toString().trim(),
+                end_time.getText().toString().trim(),
+                event_date.getText().toString().trim(),
+                Integer.parseInt(min_number.getText().toString().trim()),
+                Integer.parseInt(max_number.getText().toString().trim()),
+                Integer.parseInt(min_age.getText().toString().trim()),
+                Integer.parseInt(max_age.getText().toString().trim()),
+                event_desc.getText().toString().trim()
+        );
+        DatabaseReference myRef = database.getReference("events").push();
+        myRef.setValue(event);
     }
 }
---
+++ app/src/main/java/lf/lookingfor/Event.java
@@ -0,0 +1,107 @@
+package lf.lookingfor;
+
+public class Event {
+    private String name;
+    private String startTime;
+    private String endTime;
+    private String date;
+    private int minParticipants;
+    private int maxParticipants;
+    private int ageMin;
+    private int ageMax;
+    private String description;
+    //private String category;
+
+    public Event(String name, String startTime, String endTime, String date, int minParticipants, int maxParticipants, int ageMin, int ageMax, String description) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.date = date;
+        this.minParticipants = minParticipants;
+        this.maxParticipants = maxParticipants;
+        this.ageMin = ageMin;
+        this.ageMax = ageMax;
+        this.description = description;
+        //this.category = category;
+    }
+
+    public int getMinParticipants() {
+        return minParticipants;
+    }
+
+    public void setMinParticipants(int minParticipants) {
+        this.minParticipants = minParticipants;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public void setMaxParticipants(int maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
+
+    public int getAgeMin() {
+        return ageMin;
+    }
+
+    public void setAgeMin(int ageMin) {
+        this.ageMin = ageMin;
+    }
+
+    public int getAgeMax() {
+        return ageMax;
+    }
+
+    public void setAgeMax(int ageMax) {
+        this.ageMax = ageMax;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+   // public String getCategory() {
+    //    return category;
+    //}
+
+    //public void setCategory(String category) {
+    //    this.category = category;
+    //}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
