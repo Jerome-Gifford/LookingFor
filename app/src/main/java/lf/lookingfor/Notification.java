de2fb51458217fc4e3ae6bc2ecf5b010c24e796f
Aaron Kowalski <amkowals@svsu.edu>
2019-04-23 16:31:21 -0400

    Add notification class
    add notification class

---
+++ app/src/main/java/lf/lookingfor/Notification.java
@@ -0,0 +1,81 @@
+package lf.lookingfor;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Notification implements Parcelable {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    private  String token;
+    private String uid;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.message);
+        dest.writeString(this.token);
+        dest.writeString(this.uid);
+    }
+
+    protected Notification(Parcel in) {
+        message = in.readString();
+        token = in.readString();
+        uid = in.readString();
+    }
+
+    public static final Creator<Notification> CREATOR = new Creator<Notification>() {
+        @Override
+        public Notification createFromParcel(Parcel in) {
+            return new Notification(in);
+        }
+
+        @Override
+        public Notification[] newArray(int size) {
+            return new Notification[size];
+        }
+    };
+
+    public Notification (){
+
+    }
+
+    public Notification(Notification copyNotification){
+        this.message = copyNotification.message;
+        this.token = copyNotification.token;
+        this.uid = copyNotification.uid;
+    }
+
+    public Notification(String message, String token, String uid){
+        this.message = message;
+        this.token = token;
+        this.uid = uid;
+    }
+}
